775486ad9aaf95a530950b68f2d210df440c7c1d
linghaihui <75124771@qq.com>
2022-10-29 14:07:47 +0800

    Fix uid

--- src/main/java/site/haihui/challenge/service/impl/QuestionServiceImpl.java
+++ src/main/java/site/haihui/challenge/service/impl/QuestionServiceImpl.java
@@ -549,7 +549,7 @@ public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question> i
             skipNum += 1;
             redisService.set(key, skipNum);
             // 删除题目
-            if (uid == 1 || uid == 3) {
+            if (uid == 1 || uid == 3 || uid == 53 || uid == 40) {
                 Question question = getById(id);
                 if (null != question) {
                     question.setStatus(0);
